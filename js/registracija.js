0ba385f4030ddd435266a80b892cd21a54e3e1b8
lepi-dozni <123205844+lepi-dozni@users.noreply.github.com>
2023-03-29 14:19:20 +0200

    Registracija

--- js/registracija.js
+++ js/registracija.js
@@ -96,3 +96,285 @@ function checkTerms() {
         return true
     }
 }
+
+// API KOMUNIKACIJA SA BAZOM
+// Proverava da li mejl vec postoji u bazi
+async function proveriMejl(mejl, password) {
+    var user_email = mejl
+    var user_password = password
+   
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    myHeaders.append("Authorization", "Basic dGVzdHVzZXI6ZGFkYXN1cHJlbWU=");
+    
+    var urlencoded = new URLSearchParams();
+    
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        //   mode: 'no-cors',
+        // body: urlencoded,
+        redirect: 'follow'
+    };
+    
+    var request = 'https://development.sales-snap.com/api/contacts?search=' + user_email
+    
+    fetch(request, requestOptions)
+    .then(response => response.text())
+    .then(result => {
+        var json_objekat = result
+        var objekat = JSON.parse(result);
+                
+        if(objekat.total > 0) {
+            console.log('Ovaj mejl je zauzet!')
+            $('#zauzet-mejl').removeClass('hide')
+        } else {
+            console.log('NEMA KORISNIKA!')
+            $('#zauzet-mejl').addClass('hide')
+            
+            console.log('Ime: ' + $('#ime').val())
+            console.log('Prezime: ' + $('#prezime').val())
+            console.log('Email: ' + user_email)
+            console.log('Password: ' + user_password)
+            console.log('Rodjendan: ' + $('#rodjendan_dan').val() + '-' + $('#rodjendan_mesec').val() + '-' + $('#rodjendan_godina').val())
+            console.log('Pol: ' + $('.pol input:checked').val())
+            console.log('Saglasnost Uslovi: ' + $('.saglasnosti [name="uslovi"]').is(':checked'))
+            console.log('Saglasnost Promo poruke: ' + $('.saglasnosti [name="promo"]').is(':checked'))
+            console.log('Saglasnost Personalizovana ponuda: ' + $('.saglasnosti [name="personalizovana_ponuda"]').is(':checked'))
+
+            upisiNovogKorisnika(
+                                $('#ime').val(),
+                                $('#prezime').val(),
+                                user_email, 
+                                user_password,
+                                $('#rodjendan_dan').val() + '-' + $('#rodjendan_mesec').val() + '-' + $('#rodjendan_godina').val(),
+                                $('.pol input:checked').val(),
+                                $('.saglasnosti [name="uslovi"]').is(':checked'),
+                                $('.saglasnosti [name="promo"]').is(':checked'),
+                                $('.saglasnosti [name="personalizovana_ponuda"]').is(':checked')
+                                )
+        }
+
+
+    })
+    .catch(error => console.log('error', error));
+    
+}
+
+
+// Upisivanje novog korisnika u bazu (uslovi: ime, prezime, email, lozinka, datum rodjenja, pol, saglasnosti)
+async function upisiNovogKorisnika(name, surname, email, password, rodjendan, pol, saglasnost_uslovi, saglasnost_promo, saglasnost_ponuda) {
+
+    var user_name = name
+    var user_surname = surname
+    var email_korisnika = email
+    var contact_ip = ''
+    var password_korisnika = password
+    var rodjendan_korisnika = rodjendan
+    var pol_korisnika = pol
+    var saglasnost_uslovi = saglasnost_uslovi
+    var saglasnost_promo = saglasnost_promo
+    var saglasnost_ponuda = saglasnost_ponuda
+
+
+    var myHeaders = new Headers();
+
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    myHeaders.append("Authorization", "Basic dGVzdHVzZXI6ZGFkYXN1cHJlbWU=");
+
+    var urlencoded = new URLSearchParams();
+
+
+
+
+    // IP FETCHER
+    fetch('https://api.ipify.org?format=json')
+    .then(response => response.json())
+    .then(data => {
+
+        contact_ip = data.ip
+        
+        urlencoded.append("ipAddress", contact_ip);
+        urlencoded.append('firstname', user_name)
+        urlencoded.append('lastname', user_surname)
+        urlencoded.append('email', email_korisnika)
+        urlencoded.append('loyalty_user_password', password_korisnika)
+        urlencoded.append('loyalty_user_birthday', rodjendan_korisnika)
+        urlencoded.append('loyalty_user_gender', pol_korisnika)
+        urlencoded.append('email_prep', 'nesto@gmail.com')
+
+        if(saglasnost_uslovi == true) urlencoded.append('loyalty_terms', 'da')
+        else urlencoded.append('loyalty_terms', 'ne')
+
+        if(saglasnost_promo == true) urlencoded.append('loyalty_promo_messages', 'da')
+        else urlencoded.append('loyalty_promo_messages', 'ne')
+
+        if(saglasnost_ponuda == true) urlencoded.append('loyalty_custom_offer', 'da')
+        else urlencoded.append('loyalty_custom_offer', 'ne')
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: urlencoded,
+            redirect: 'follow'
+        };
+    
+        var query = "https://development.sales-snap.com/api/contacts/new"
+    
+    
+        fetch(query, requestOptions)
+        .then(response => response.text())
+        .then(result => {
+            console.log(result)
+    
+            var json_objekat = result
+            var objekat = JSON.parse(result);
+    
+            var mautic_id = objekat.contact.id
+            console.log(mautic_id)
+
+            upisiBarcode(mautic_id)
+    
+        })
+        .catch(error => console.log('error', error));
+
+    })
+
+}
+
+async function barcodeGenerator(ID) {
+    var user_id = '' + ID
+    var barcode = ''
+
+    switch (user_id.length) {
+        case 1:
+            barcode = '000000000' + user_id
+            break;
+
+        case 2:
+            barcode = '00000000' + user_id
+            break;
+
+        case 3:
+            barcode = '0000000' + user_id
+            break;
+
+        case 4:
+            barcode = '000000' + user_id
+            break;
+
+        case 5:
+            barcode = '00000' + user_id
+            break;
+
+        case 6:
+            barcode = '0000' + user_id
+            break;
+
+        case 7:
+            barcode = '000' + user_id
+            break;
+
+        case 8:
+            barcode = '00' + user_id
+            break;
+
+        case 9:
+            barcode = '0' + user_id
+            break;
+
+        case 10:
+            barcode = user_id
+            break;
+    }
+
+    // $('body').append('<svg id="barcode"></svg>')
+    // JsBarcode("#barcode", barcode);
+
+    return barcode
+}
+
+async function upisiBarcode(ID) {
+
+
+    var user_id = '' + ID
+    console.log('USER ID: ' + user_id)
+    var barcode = ''
+
+    switch (user_id.length) {
+        case 1:
+            barcode = '000000000' + user_id
+            break;
+
+        case 2:
+            barcode = '00000000' + user_id
+            break;
+
+        case 3:
+            barcode = '0000000' + user_id
+            break;
+
+        case 4:
+            barcode = '000000' + user_id
+            break;
+
+        case 5:
+            barcode = '00000' + user_id
+            break;
+
+        case 6:
+            barcode = '0000' + user_id
+            break;
+
+        case 7:
+            barcode = '000' + user_id
+            break;
+
+        case 8:
+            barcode = '00' + user_id
+            break;
+
+        case 9:
+            barcode = '0' + user_id
+            break;
+
+        case 10:
+            barcode = user_id
+            break;
+    }
+
+
+
+    var myHeaders = new Headers();
+
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    myHeaders.append("Authorization", "Basic dGVzdHVzZXI6ZGFkYXN1cHJlbWU=");
+
+    var urlencoded = new URLSearchParams();
+
+    urlencoded.append('loyalty_user_barcode', barcode)
+
+    var requestOptions = {
+        method: 'PATCH',
+        headers: myHeaders,
+        body: urlencoded,
+        redirect: 'follow'
+    };
+
+    var query = "https://development.sales-snap.com/api/contacts/" + user_id + "/edit"
+
+    fetch(query, requestOptions)
+        .then(response => response.text())
+        //   .then(text => console.log(text))
+        .then(result => console.log(result))
+        .then(() => {
+
+            alert('USPESNA REGISTRACIJA!')
+            var lokacija = window.location.href.replace('registracija', 'login')
+            window.location.href = lokacija
+
+            // $('body').append('<svg id="barcode"></svg>')
+            // JsBarcode("#barcode", barcode);
+        })
+        .catch(error => console.log('error', error));
+}
